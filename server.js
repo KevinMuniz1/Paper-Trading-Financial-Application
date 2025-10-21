36b5b38443f393324fa993d267332868334682d4
Kevin Muniz <muniz.kevin@outlook.com>
2025-10-21 11:44:12 -0400

    test change

--- server.js
+++ server.js
@@ -92,4 +92,8 @@ app.get('/', (req, res) => {
   res.send('Server is alive!');
 });
 
-app.listen(5050, '0.0.0.0', () => console.log("Server running on all interfaces"));
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
