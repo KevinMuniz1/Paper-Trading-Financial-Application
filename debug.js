27565f008f783ba63421366576dd0dc964bc6bde
rggleal <rebecagonzalez.leal@gmail.com>
2025-11-05 03:36:56 -0500

    set up resend email sending service + jwt token generation dependencies

--- debug.js
+++ debug.js
@@ -3,6 +3,9 @@ console.log('Environment variables loaded:');
 console.log('PORT:', process.env.PORT);
 console.log('MONGODB_URL:', process.env.MONGODB_URL);
 console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('JWT Secret:', process.env.JWT_SECRET);
+console.log('Resend API Key exists:', !!process.env.RESEND_API_KEY);
+
 
 // Read config
 const { PORT, MONGODB_URL } = require('./config');
